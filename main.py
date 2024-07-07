d208be731f79866b4862d4c51a2684175a5d3d90
FedFedavi <kykypyk@gmail.com>
2024-07-07 21:48:23 +0400

    add link to img

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("image/tier.jpg")
 
 running = True
 while running:
